d8c7516c07052fd2b20a62d85e9236791d85635b
ccimage <zhangchuan.cn@gmail.com>
2026-09-11 15:00:18 +0800

    🐛 FIX: 修改build脚本

--- startExe.ps1
+++ startExe.ps1
@@ -1,2 +1,2 @@
 dotnet publish .\src\MediaForge.App\MediaForge.App.csproj -c Release -p:Platform=x64
-.\artifacts\publish\win-x64\MediaForge.exe
+.\artifacts\publish\framework-dependent-win-x64\MediaForge.exe
