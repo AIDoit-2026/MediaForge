81bd2dba51f2234fcc6276ec127aaee3359b430d
ccimage <zhangchuan.cn@gmail.com>
2026-09-11 18:52:54 +0800

    🐛 FIX: 编译和运行脚本分开

---
+++ releaseExe.ps1
@@ -0,0 +1,1 @@
+dotnet publish .\src\MediaForge.App\MediaForge.App.csproj -c Release -p:Platform=x64
--- startExe.ps1
+++ startExe.ps1
@@ -1,2 +1,1 @@
-dotnet publish .\src\MediaForge.App\MediaForge.App.csproj -c Release -p:Platform=x64
 .\artifacts\publish\framework-dependent-win-x64\MediaForge.exe
